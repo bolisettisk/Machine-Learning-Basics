4eb89e071b733889adb48eeb4dc0a4d6b44bf2e0
bolisettisk <siva.karteek@gmail.com>
2021-12-30 00:54:01 +0000

    .

--- section5.R
+++ section5.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(dslabs)
+library(dplyr)
 library(caret)
 library(ggplot2)
 data("olive")
@@ -12,3 +13,23 @@ olive <- olive %>% select(-area)
 
 fit <- train(region ~., method = "knn", tuneGrid = data.frame(k = seq(1, 15, 2)), data = olive)
 ggplot(fit)
+
+dat <- olive %>% pivot_longer(-"region", names_to = "comp", values_to = "values")
+dat %>% group_by(comp) %>% ggplot(aes(region,values, fill = region)) + geom_boxplot() + facet_wrap(~comp, scale = "free", ncol = 4)
+
+olive %>% ggplot(aes(eicosenoic, linoleic, color = region)) + geom_point()
+
+#-------------------------
+
+data("polls_2008")
+qplot(day, margin, data = polls_2008)
+
+
+
+
+
+
+
+
+
+
