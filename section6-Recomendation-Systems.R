f8c372ac6869df77012680937b2377f07ceeee64
bolisettisk <siva.karteek@gmail.com>
2022-01-16 23:03:30 +0000

    .

---
+++ section6-Recomendation-Systems.R
@@ -0,0 +1,56 @@
+library(tidyverse)
+library(dslabs)
+library(dplyr)
+library(caret)
+library(ggplot2)
+library(rpart)
+library(matrixStats)
+library(randomForest)
+library(Rborist)
+
+data("movielens")
+
+cat("\014")
+head(movielens)
+
+cat("\014")
+movielens %>%
+  summarize(n_users = n_distinct(userId),
+            n_movies = n_distinct(movieId))
+
+keep <- movielens %>%
+  dplyr::count(movieId) %>%
+  top_n(5) %>%
+  pull(movieId)
+
+tab <- movielens %>%
+  filter(userId %in% c(13:20)) %>% 
+  filter(movieId %in% keep) %>% 
+  select(userId, title, rating) %>% 
+  spread(title, rating)
+tab %>% knitr::kable()
+
+cat("\014")
+users <- sample(unique(movielens$userId), 100)
+rafalib::mypar()
+movielens %>% filter(userId %in% users) %>% 
+  select(userId, movieId, rating) %>%
+  mutate(rating = 1) %>%
+  spread(movieId, rating) %>% select(sample(ncol(.), 100)) %>% 
+  as.matrix() %>% t(.) %>%
+  image(1:100, 1:100,. , xlab="Movies", ylab="Users")
+abline(h=0:100+0.5, v=0:100+0.5, col = "grey")
+
+movielens %>% 
+  dplyr::count(movieId) %>% 
+  ggplot(aes(n)) + 
+  geom_histogram(bins = 30, color = "black") + 
+  scale_x_log10() + 
+  ggtitle("Movies")
+
+movielens %>%
+  dplyr::count(userId) %>% 
+  ggplot(aes(n)) + 
+  geom_histogram(bins = 30, color = "black") + 
+  scale_x_log10() +
+  ggtitle("Users")
--- section6.R
+++ section6.R
@@ -45,7 +45,7 @@ colnames(x_test) <- colnames(x)
 # Commented since it takes to too long to execute
 # control <- trainControl(method = "cv", number = 10, p = 0.9)
 # train_knn <- train(x[, col_index], y, method = "knn", tuneGrid = data.frame(k = c(3, 5, 7)), trControl = control)
-# 
+#
 # cat("\014")
 # train_knn
 
@@ -72,7 +72,7 @@ control <- trainControl(method = "cv", number = 5, p = 0.8)
 grid <- expand.grid(minNode = c(1,5), predFixed = c(10, 15, 25, 35, 50))
 
 # Commented since it takes to too long to execute
-# train_rf <- train(x[ , col_index], y, method = "Rborist", nTree = 50, trControl = control, tuneGrid = grid, nSamp = 5000)
+train_rf <- train(x[ , col_index], y, method = "Rborist", nTree = 50, trControl = control, tuneGrid = grid, nSamp = 5000)
 
 ggplot(train_rf)
 train_rf$bestTune
@@ -86,10 +86,58 @@ cm$overall["Accuracy"]
 
 rafalib::mypar(3,4)
 for(i in 1:12){
-  image(matrix(x_test[i,], 28, 28)[, 28:1], 
+  image(matrix(x_test[i,], 28, 28)[, 28:1],
         main = paste("Our prediction:", y_hat_rf[i]),
         xaxt="n", yaxt="n")
 }
+
+
+cat("\014")
+# rf <- randomForest(x, y, ntree = 50)
+imp <- importance(rf)
+
+image(matrix(imp, 28, 28))
+
+p_max <- predict(fit_knn, x_test[,col_index])
+p_max <- apply(p_max, 1, max)
+ind  <- which(y_hat_knn != y_test)
+ind <- ind[order(p_max[ind], decreasing = TRUE)]
+rafalib::mypar(3,4)
+for(i in ind[1:12]){
+  image(matrix(x_test[i,], 28, 28)[, 28:1],
+        main = paste0("Pr(",y_hat_knn[i],")=",round(p_max[i], 2),
+                      " but is a ",y_test[i]),
+        xaxt="n", yaxt="n")
+}
+
+p_max <- predict(fit_rf, x_test[,col_index])$census
+p_max <- p_max / rowSums(p_max)
+p_max <- apply(p_max, 1, max)
+ind  <- which(y_hat_rf != y_test)
+ind <- ind[order(p_max[ind], decreasing = TRUE)]
+rafalib::mypar(3,4)
+for(i in ind[1:12]){
+  image(matrix(x_test[i,], 28, 28)[, 28:1],
+        main = paste0("Pr(",y_hat_rf[i],")=",round(p_max[i], 2),
+                      " but is a ",y_test[i]),
+        xaxt="n", yaxt="n")
+}
+
+
+p_rf <- predict(fit_rf, x_test[,col_index])$census
+p_rf <- p_rf / rowSums(p_rf)
+p_knn <- predict(fit_knn, x_test[,col_index])
+p <- (p_rf + p_knn)/2
+y_pred <- factor(apply(p, 1, which.max)-1)
+cat("\014")
+confusionMatrix(y_pred, y_test)
+################ Uncomment from here to line 30
+
+
+
+
+
+
 
 
 
