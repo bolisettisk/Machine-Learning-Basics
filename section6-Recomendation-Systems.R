9f7672a8c2c1ca3545551c61f98c1d60f7700810
bolisettisk <siva.karteek@gmail.com>
2022-01-17 19:15:08 +0000

    .

--- section6-Recomendation-Systems.R
+++ section6-Recomendation-Systems.R
@@ -54,3 +54,23 @@ movielens %>%
   geom_histogram(bins = 30, color = "black") + 
   scale_x_log10() +
   ggtitle("Users")
+
+
+cat("\014")
+set.seed(755)
+test_index <- createDataPartition(y = movielens$rating, times = 1, p = 0.2, list = FALSE)
+train_set <- movielens[-test_index,]
+test_set <- movielens[test_index,]
+test_set <- test_set %>% semi_join(train_set, by = "movieId") %>% semi_join(train_set, by = "userId")
+ 
+RMSE <- function(true_ratings, predicted_ratings){
+  sqrt(mean((true_ratings - predicted_ratings)^2))
+}
+
+
+
+
+
+
+
+
