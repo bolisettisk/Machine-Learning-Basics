d77b53a6632e1debb63bdd23987c656fc6f926a1
bolisettisk <siva.karteek@gmail.com>
2022-01-17 20:59:22 +0000

    .

--- section6-Recomendation-Systems.R
+++ section6-Recomendation-Systems.R
@@ -7,6 +7,7 @@ library(rpart)
 library(matrixStats)
 library(randomForest)
 library(Rborist)
+library(ggplot2)
 
 data("movielens")
 
@@ -68,6 +69,48 @@ RMSE <- function(true_ratings, predicted_ratings){
 }
 
 
+cat("\014")
+mu_hat <- mean(train_set$rating)
+# mu_hat
+naive_rmse <- RMSE(test_set$rating, mu_hat)
+# naive_rmse
+# predictions <- rep(2.5, nrow(test_set)) # checking for a value other than mu_hat
+# RMSE(test_set$rating, predictions)
+
+rmse_results <- data_frame(method = "just the average", RMSE = naive_rmse)
+
+
+# Modelling movie effect
+cat("\014")
+# fit <- lm(rating ~ as.factor(movieId), data = movielens)
+mu <- mean(train_set$rating)
+movie_avgs <- train_set %>% group_by(movieId) %>% summarise(b_i = mean(rating - mu))
+movie_avgs %>% qplot(b_i, geom ="histogram", bins = 10, data = ., color = I("black"))
+predicted_ratings <- mu + test_set %>% left_join(movie_avgs, by="movieId") %>% .$b_i
+model_1_rmse <- RMSE(predicted_ratings, test_set$rating)
+
+cat("\014")
+rmse_results <- bind_rows(rmse_results, data_frame(method = "Movie Effect Model", RMSE = model_1_rmse))
+rmse_results %>% knitr::kable()
+
+# Modelling user effect
+train_set %>% group_by(userId) %>% summarise(b_u = mean(rating)) %>% filter(n() >= 100) %>% ggplot(aes(b_u)) + geom_histogram(bins = 30, color = "black")
+
+# lm(rating ~ as.factor(movieId) + as.factor(userId))
+user_avgs <- train_set %>% left_join(movie_avgs, by = "movieId") %>% group_by(userId) %>% summarise(b_u = mean(rating - mu - b_i))
+
+predicted_ratings <- test_set %>%
+  left_join(movie_avgs, by='movieId') %>%
+  left_join(user_avgs, by='userId') %>%
+  mutate(pred = mu + b_i + b_u) %>%
+  .$pred
+
+model_2_rmse <- RMSE(predicted_ratings, test_set$rating)
+rmse_results <- bind_rows(rmse_results,
+                          data_frame(method="Movie + User Effects Model",
+                                     RMSE = model_2_rmse ))
+cat("\014")
+rmse_results %>% knitr::kable()
 
 
 
